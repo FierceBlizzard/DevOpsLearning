7935ee0ee0b739663f8794446101f844de2e1091
fierceblizzard11 <fierceblizzard11@gmail.com>
2022-11-21 17:31:17 -0500

    i think I can run tests now

--- smath/src/test/AppTest.java
+++ smath/src/test/AppTest.java
@@ -1,24 +1,26 @@
-package smath.src.test.java.iclyn;
+package smath.src.test;
 
 import org.junit.Assert.*;
-
-import smath.src.main.java.iclyn.App;
-
 import static org.junit.Assert.assertEquals;
-
+import java.beans.Transient;
 import org.junit.Test;
+import App;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void math()
-    {
+    public void math(){
         assertEquals(App.math(1, 1, 1), 3);
+    }
+}
+public class TryingTest{
+    @Test
+    public void mathTest(){
+        assertEquals(App.math(1,1,1), 3);
     }
 }
